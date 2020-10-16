25a6007c20c2951447e59f75a87528b535c5a31f
Gerolf Ziegenhain <gerolfziegenhain@DFS-Gerolf-MacBook-89.local>
2020-10-16 21:36:41 +0200

    works

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #docker run --rm -it --volume="$PWD:/srv/jekyll" --volume="jekyll_bundle:/usr/local/bundle" --volume="jekyll_gems:/usr/gem" --env JEKYLL_ENV=production jekyll/jekyll jekyll build
-docker run --rm -it --volume="$PWD:/srv/jekyll" --volume="jekyll_bundle:/usr/local/bundle" --volume="jekyll_gems:/usr/gem" --env JEKYLL_ENV=production  -p 4000:4000 jekyll/jekyll jekyll serve
+docker run --rm -it --volume="$PWD:/srv/jekyll" --volume="jekyll_bundle:/usr/local/bundle" --volume="jekyll_gems:/usr/gem" --env JEKYLL_ENV=production  -p 4000:4000 jekyll/jekyll:3.8.5 jekyll serve
