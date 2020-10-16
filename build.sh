c9981e6938b9f9587f099743c25f372eb2859937
Gerolf Ziegenhain <gerolfziegenhain@DFS-Gerolf-MacBook-89.local>
2020-10-16 20:56:11 +0200

    clean

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm -it --volume="$PWD:/srv/jekyll" --volume="$PWD/vendor/bundle:/usr/local/bundle" --env JEKYLL_ENV=production jekyll/jekyll jekyll build
+docker run --rm -it --volume="$PWD:/srv/jekyll" --volume="$PWD/vendor/bundle:/usr/local/bundle" --volume="$PWD/vendor/gems:/usr/gem" --env JEKYLL_ENV=production jekyll/jekyll jekyll build
