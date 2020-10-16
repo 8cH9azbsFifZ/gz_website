151018d85587ebe3a98bc8ed1d0a9f0fb70561a7
Gerolf Ziegenhain <gerolfziegenhain@DFS-Gerolf-MacBook-89.local>
2020-10-16 21:51:21 +0200

    new deployment

---
+++ _site/deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+git commit -a -m "new deployment"
+git push
+git checkout gh-pages
+git rm -rf .
+git checkout jekyll -- _site
+git mv _site/* .	
+rmdir _site
+git add .
+git commit -a -m "new deployment"
+git push
+git checkout jekyll
