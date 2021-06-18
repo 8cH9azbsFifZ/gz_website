90363826ea8fe10c4d0db7350e07317ae1c882ba
8cH9azbsFifZ <g@ziegenhain.com>
2021-06-18 14:24:49 +0200

    new deployment

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,10 @@ git commit -a -m "new deployment"
 git push
 git checkout gh-pages
 git rm -rf .
-git checkout jekyll -- _site
+git checkout master -- _site
 git mv _site/* .	
 rmdir _site
 git add .
 git commit -a -m "new deployment"
 git push
-git checkout jekyll
+git checkout master
